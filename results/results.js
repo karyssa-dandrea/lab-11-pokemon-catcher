f8f6efb9a9ede90971e23904027fe47ae69c8b9d
karyssa-dandrea <karyssadandrea@gmail.com>
2021-10-22 14:33:01 -0700

    added some styling and more data to my chart

--- results/results.js
+++ results/results.js
@@ -15,7 +15,7 @@ for (let item of results){
     const header = document.createElement('h2');
     header.textContent = poke.name;
     const resultsSpan1 = document.createElement('span');
-    resultsSpan1.textContent = `Shown: ${item.shown}`;
+    resultsSpan1.textContent = `Shown: ${item.shown} `;
     const resultsSpan2 = document.createElement('span');
     resultsSpan2.textContent = `Picked: ${item.catch}`;
 
@@ -30,6 +30,8 @@ const names = results.map((item)=>{
 });
 
 const catchPoke = results.map(item=>item.catch);
+
+const shown = results.map(item=>item.shown);
 
 var ctx = document.getElementById('resultsChart').getContext('2d');
 // eslint-disable-next-line no-undef
@@ -57,8 +59,29 @@ new Chart(ctx, {
                 'rgba(255, 159, 64, 1)'
             ],
             borderWidth: 1
-        }]
-    },
+        }, {
+            type: 'bar',
+            label: '# of Times Shown',
+            data: shown, 
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }],
+    }, 
     options: {
         scales: {
             y: {
